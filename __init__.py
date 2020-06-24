dfb9710ffcfe31feff5945ede75f0ddfa9c9c2f2
Colette Salyk <csalyk@gmail.com>
2020-06-24 10:27:03 -0400

    Added routines to init

--- __init__.py
+++ __init__.py
@@ -4,4 +4,8 @@ from .spec_utils import line_overlap_mband
 from .spec_utils import spec_combine
 from .spec_utils import read_irs
 from .spec_utils import vproj
+from .spec_utils import vhelio_to_vlsr
+from .spec_utils import vlsr_to_vhelio
+
+
 
